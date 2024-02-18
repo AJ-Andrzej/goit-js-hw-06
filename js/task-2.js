93908a82c7988352d74172827d89ca32326399a6
AJ-Andrzej <aayaschenko@gmail.com>
2024-02-18 20:11:42 +0100

    task-2

---
+++ js/task-2.js
@@ -0,0 +1,25 @@
+class Storage {
+    constructor(items) {
+        this.items = items;
+    }
+
+    getItems() {
+        return this.items;
+    }
+    addItem(newItem) {
+        this.items.push(newItem)
+    }
+    removeItem(itemToRemove) {
+        const deleteItemIndex = this.items.indexOf(itemToRemove)
+        if (deleteItemIndex !== -1) {
+            this.items.splice(deleteItemIndex, 1)
+        }
+    }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
